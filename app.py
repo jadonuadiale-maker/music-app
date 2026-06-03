3b4272f169281637ee8ec04fa8bcac040ea37165
Jadon Victor-Uadiale <jadonuadiale@gmail.com>
2026-06-03 17:05:42 +0100

    Added playback controls

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from os import environ
+environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
 import pygame
 
 pygame.mixer.init()
@@ -5,6 +7,15 @@ pygame.mixer.init()
 def play_music(file_path):
     pygame.mixer.music.load(file_path)
     pygame.mixer.music.play()
+
+def pause_music():
+    pygame.mixer.music.pause()
+
+def resume_music():
+    pygame.mixer.music.unpause()
+
+def stop_music():
+    pygame.mixer.music.stop()
 
 if __name__ == "__main__":
     print("Music App Ready")
---
+++ test.py
@@ -0,0 +1,10 @@
+from app import play_music, pause_music, resume_music, stop_music
+import time
+
+play_music('Ron-Be Glorified.mp3')
+time.sleep(5)
+pause_music()
+time.sleep(2)
+resume_music()
+time.sleep(5)
+stop_music()
